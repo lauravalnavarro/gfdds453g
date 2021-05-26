39b31980104d686887941cf60d40e3b99c28a5cb
Laura <lvna.95@gmail.com>
2021-05-25 19:49:31 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 import {
   Switch,
   Route,
-  Redirect,
-  Link
+  Link, 
+  Redirect
 } from 'react-router-dom';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          {/*Aca deben ir los links de navegacion*/}
-        </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
-      </div>
+    
+        <div>
+          <nav>
+            {/*Aca deben ir los links de navegacion*/}
+           
+                <Link to="/page1">Página 1</Link>
+              
+                <Link to="/page2">Página 2</Link>
+            
+          </nav>
+          {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+          <Switch>
+         
+            <Route exact path="/page1">
+                <Page1 />
+            </Route>
+            <Route path="/page2">
+                <Page2 />
+            </Route>
+            <Redirect exact from="/" to="/page1" />
+            
+            <Route path='*' component={NotFound} />
+
+          </Switch>
+          
+
+        </div>
+     
+     
     )
   }
 }
